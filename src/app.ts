0edf949795d50a3ee85f4d783db9f835c6069291
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-06-22 09:11:56 +0600

    Created User registration api

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,75 @@ import express, { Application, Request, Response } from "express";
 import config from "./config";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import httpStatus from "http-status";
 import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 const app: Application = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: config.app_url,
-    credentials: true
-}));
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: `Express Server is running on Port: ${config.port}`,
+    author: "Tamim Khan",
+    learningTopic: "Prisma",
+  });
+});
 
-app.get('/', async (req: Request, res: Response) => {
-    res.status(200).json({
-        message: `Express Server is running on Port: ${config.port}`,
-        author: 'Tamim Khan',
-        learningTopic: 'Prisma'
-    })
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const { name, email, password, profilePhoto } = req.body;
+
+  const isUserExist = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (isUserExist) {
+    throw new Error("User with this email already exists!");
+  }
+
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  await prisma.profile.create({
+    data: {
+      userId: createdUser.id,
+      profilePhoto,
+    },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: { id: createdUser.id, email: createdUser.email || email },
+    include: { profile: true },
+    omit: { password: true }
+  });
+
+  res
+    .status(httpStatus.CREATED)
+    .json({ 
+        success: true,
+        statusCode: httpStatus.CREATED,
+        message: "User registered successfully",
+        data: user
+    });
 });
 
 export default app;
