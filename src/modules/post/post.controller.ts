2c2164cd2fff868de869a8b55e10b04b06ca6b65
Tamim Khan <md.tamim.iq.khan@gmail.com>
2026-06-29 08:59:49 +0600

    Created getMyPosts and getPostById function

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -4,7 +4,8 @@ import { postService } from "./post.service";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status";
 
-const createPost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const createPost = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const id = req.user?.id;
 
     const payload = req.body;
@@ -12,45 +13,79 @@ const createPost = catchAsync(async (req: Request, res: Response, next: NextFunc
     const result = await postService.createPostIntoDB(payload, id as string);
 
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.CREATED,
-        message: "Post created successfully",
-        data: result
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "Post created successfully",
+      data: result,
     });
-});
+  },
+);
 
-const getAllPosts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+const getAllPosts = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const result = await postService.getAllPostsFromDB();
 
     sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: "Posts Retrieved successfully",
-        data: result
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Posts Retrieved successfully",
+      data: result,
     });
-});
+  },
+);
 
-const getPostsStats = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-});
+const getPostsStats = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {},
+);
 
-const getMyPosts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-});
+const getMyPosts = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const authorId = req.user?.id;
 
-const getPostById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-});
+    const result = await postService.getMyPostsFromDB(authorId as string);
 
-const updatePost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-});
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "My Posts Retrieved successfully",
+      data: result,
+    });
+  },
+);
 
-const deletePost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-});
+const getPostById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const postId = req.params.postId;
+
+    if (!postId) {
+      throw new Error("Post ID is required In Params");
+    }
+
+    const result = await postService.getPostByIdFromDB(postId as string);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Post Retrieved successfully",
+      data: result,
+    });
+  },
+);
+
+const updatePost = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {},
+);
+
+const deletePost = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {},
+);
 
 export const postController = {
-    createPost,
-    getAllPosts,
-    getPostsStats,
-    getMyPosts,
-    getPostById,
-    updatePost,
-    deletePost,
+  createPost,
+  getAllPosts,
+  getPostsStats,
+  getMyPosts,
+  getPostById,
+  updatePost,
+  deletePost,
 };
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,41 +1,72 @@
 import { prisma } from "../../lib/prisma";
 import { ICreatePostPayload } from "./post.interface";
 
-const createPostIntoDB = async (payload: ICreatePostPayload, userId: string) => {
-    const result = await prisma.post.create({ 
-        data: {
-            ...payload,
-            authorId: userId
-        }
-    });
+const createPostIntoDB = async (
+  payload: ICreatePostPayload,
+  userId: string,
+) => {
+  const result = await prisma.post.create({
+    data: {
+      ...payload,
+      authorId: userId,
+    },
+  });
 
-    return result;
+  return result;
 };
 
 const getAllPostsFromDB = async () => {
-    const posts = await prisma.post.findMany({
-        include: { author: { omit: { password: true } }, comments: true }
-    });
+  const posts = await prisma.post.findMany({
+    include: { author: { omit: { password: true } }, comments: true },
+  });
 
-    return posts;
+  return posts;
 };
 
 const getPostsStats = async () => {};
 
-const getMyPostsFromDB = async () => {};
+const getMyPostsFromDB = async (authorId: string) => {
+  const result = await prisma.post.findMany({
+    where: { authorId },
+    orderBy: { createdAt: "desc" },
+    include: {
+      comments: true,
+      author: { omit: { password: true } },
+      _count: {
+        select: {
+          comments: true,
+        },
+      },
+    },
+  });
 
-const getPostByIdFromDB = async () => {};
+  return result;
+};
+
+const getPostByIdFromDB = async (postId: string) => {
+  const post = await prisma.post.findUniqueOrThrow({
+    where: { id: postId },
+  });
+
+  const updatedPost = await prisma.post.update({
+    where: { id: postId },
+    data: { views: { increment: 1 } },
+    include: { author: { omit: { password: true } }, comments: true },
+  });
+
+  return updatedPost;
+};
 
 const updatePostInDB = async () => {};
 
 const deletePostFromDB = async () => {};
 
 export const postService = {
-    createPostIntoDB,
-    getAllPostsFromDB,
-    getPostsStats,
-    getMyPostsFromDB,
-    getPostByIdFromDB,
-    updatePostInDB,
-    deletePostFromDB
+  createPostIntoDB,
+  getAllPostsFromDB,
+  getPostsStats,
+  getMyPostsFromDB,
+  getPostByIdFromDB,
+  updatePostInDB,
+  deletePostFromDB,
 };
